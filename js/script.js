9384489d1e6e77922d8bce527c1f4d624c846d69
Zoe1903 <crystalbarkie@gmail.com>
2019-11-05 23:22:55 +0000

    click handler

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function(){
+
+let words=$("input").val();
+let word=translate(words)
+$(".output").text(`${word}`)
+
+})
+
+function translate(word) {
+ let firstLetter = word.charAt(0);
+ let theRest= word.slice(1);
+ return `${theRest}${firstLetter}ay`
+
+
+}
 
 
 
-  
-  
-  
-  
 });
